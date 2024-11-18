c8e12fa053cd17622174ba408ba4f7eee51e150f
Auston Mtabane <mtabane.auston@gmail.com>
2024-11-18 15:16:16 +0200

    section on functionality

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+let cir,dia,area,rad;
+let pi = 3.14;
 
+document.getElementById("bt-cir").onclick = function(){
+    rad = document.getElementById("in-cir").value
+    cir = 2*pi*rad
+    console.log(cir)
+    document.getElementById("ans1").textContent = `circumference: ${cir}`;
+
+}
+33.3
